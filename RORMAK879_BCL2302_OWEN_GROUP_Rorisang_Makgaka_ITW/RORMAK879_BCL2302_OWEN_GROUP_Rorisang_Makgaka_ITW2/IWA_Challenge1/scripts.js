19b97967ee55d8ce778a7914efdc565d76a00114
Matere29 <makgakarorisang@gmail.com>
2023-03-30 17:02:46 +0200

    fixed count variable, declared variables correctly

---
+++ RORMAK879_BCL2302_OWEN_GROUP_Rorisang_Makgaka_ITW/RORMAK879_BCL2302_OWEN_GROUP_Rorisang_Makgaka_ITW2/IWA_Challenge1/scripts.js
@@ -0,0 +1,14 @@
+
+
+
+
+/* This is the firstname of the user */
+
+const user = 'John'
+
+
+/* This is the lastname of the user */
+ const surname = 'Smith'
+
+//comma is used to separate argument instead of semicolon.
+console.log(user, surname);
---
+++ RORMAK879_BCL2302_OWEN_GROUP_Rorisang_Makgaka_ITW/RORMAK879_BCL2302_OWEN_GROUP_Rorisang_Makgaka_ITW2/IWA_Challenge2/scripts.js
@@ -0,0 +1,7 @@
+
+
+/* This is the date that a user created their account */
+
+const date = '10/07/2014'
+//date is displayed using console.log
+console.log(date);
