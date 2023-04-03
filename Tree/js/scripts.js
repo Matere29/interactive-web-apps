f96d180b52623b34b9bef5f76bac15c36b76fd1a
Matere29 <makgakarorisang@gmail.com>
2023-04-03 14:55:38 +0200

    Creating a tree

--- Tree/js/scripts.js
+++ Tree/js/scripts.js
@@ -1,4 +1,15 @@
-const x = '*'
+let x = '*'
 
-x = x + 1;
+const y = x + x
+const z = x + y
+const w = x + z
+const h = x + w
 
+console.log("" + x + "")
+console.log(y)
+console.log(z)
+console.log(w)
+console.log(h)
+console.log(" " + x +" ") 
+console.log(" " + x +" ")
+console.log(h) 
