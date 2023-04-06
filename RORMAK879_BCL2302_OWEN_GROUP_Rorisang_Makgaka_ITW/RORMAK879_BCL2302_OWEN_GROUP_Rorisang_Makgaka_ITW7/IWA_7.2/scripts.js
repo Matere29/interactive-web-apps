661ba5b13059f47ac3f1f62d31bc18bb0c0af06c
Matere29 <makgakarorisang@gmail.com>
2023-04-06 10:41:16 +0200

    Fixed console log message and added comments

--- RORMAK879_BCL2302_OWEN_GROUP_Rorisang_Makgaka_ITW/RORMAK879_BCL2302_OWEN_GROUP_Rorisang_Makgaka_ITW7/IWA_7.2/scripts.js
+++ RORMAK879_BCL2302_OWEN_GROUP_Rorisang_Makgaka_ITW/RORMAK879_BCL2302_OWEN_GROUP_Rorisang_Makgaka_ITW7/IWA_7.2/scripts.js
@@ -1,4 +1,19 @@
-const nickname= "Timmy";
-const firstname = "Timothy";
+const nickname= 'Timmy';
+const firstname = 'Timothy';
 
-console.log("Good Morning, ${nickname} || {firstname}!")
+/*I used if statements to check for nickname and display message
+* If nickname is null, display firstname and if both are null display Good Morning
+*/
+
+
+if(nickname){
+ console.log("Good morning!",nickname)
+}else if(firstname){
+    console.log("Good Morning!",firstname)
+}else{
+    console.log("Good morning!")
+}
+
+
+
+//console.log("Good Morning, ${nickname} || {firstname}!")
