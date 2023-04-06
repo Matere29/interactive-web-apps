e600162a4077d38358b572082c10949165a4d3c5
Matere29 <makgakarorisang@gmail.com>
2023-04-06 15:35:51 +0200

    Fixed code and added comments

--- Tree/js/scripts.js
+++ Tree/js/scripts.js
@@ -1,11 +1,10 @@
 
 let number = 1;
 let x = '*';
+//declared two variables, number which acts as a counter, and X which holds a symbol we want to display
+for(number = 1; number<=4; number++){
 
-
-while(number <= 10 ){
-  console.log(number);
-  number++;
-  
+  console.log(x);
+  x = x + '*';
   
 }
