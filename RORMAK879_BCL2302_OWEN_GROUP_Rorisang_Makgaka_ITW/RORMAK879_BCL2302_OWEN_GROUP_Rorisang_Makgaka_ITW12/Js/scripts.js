5f467c965189a112481f54c601ecd26be2f2675b
Matere29 <makgakarorisang@gmail.com>
2023-04-19 10:14:11 +0200

    Fixed code

--- RORMAK879_BCL2302_OWEN_GROUP_Rorisang_Makgaka_ITW/RORMAK879_BCL2302_OWEN_GROUP_Rorisang_Makgaka_ITW12/Js/scripts.js
+++ RORMAK879_BCL2302_OWEN_GROUP_Rorisang_Makgaka_ITW/RORMAK879_BCL2302_OWEN_GROUP_Rorisang_Makgaka_ITW12/Js/scripts.js
@@ -54,21 +54,7 @@ checkoutButtons[i].style.color = checkoutButtons[i].disabled ? 'gray' : 'black';
 checkinButtons[i].disabled = !statusInfo.canCheckIn;
 checkinButtons[i].style.color = checkinButtons[i].disabled ? 'gray' : 'black';
 }
-/*
-// Set the color of the status element
-statusElements[i].style.color = statusInfo.color;
-// Set the status of the reserve button
-reserveButtons[i].disabled = !statusInfo.canReserve;
-reserveButtons[i].style.color = reserveButtons[i].disabled ? 'gray' : 'black';
-// Set the status of the checkout button
-checkoutButtons[i].disabled = !statusInfo.canCheckout;
-checkoutButtons[i].style.color = checkoutButtons[i].disabled ? 'gray' : 'black';
-// Set the status of the checkin button
-checkinButtons[i].disabled = !statusInfo.canCheckIn;
-checkinButtons[i].style.color = checkinButtons[i].disabled ? 'gray' : 'black';
-}
 
-*/
 
 
 
